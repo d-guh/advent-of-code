d22b00391851d471fe4ba6a9052c1b060d238af3
D-Guy <ddcool535@gmail.com>
2025-12-18 00:39:01 -0500

    Completed year2025/day1

---
+++ year2025/day1/part1/solution.py
@@ -0,0 +1,30 @@
+FILE = "../.input"
+
+def main():
+    position = 50
+    count = 0
+
+    with open(FILE, 'r') as rotations_file:
+        for line in rotations_file:
+            direction = line[0]
+            value = int(line[1:])
+
+            match direction:
+                case 'L':
+                    position -= value
+                    # print(f"DEBUG: {line.strip()}: {position}")
+                case 'R':
+                    position += value
+                    # print(f"DEBUG: {line.strip()}: {position}")
+                case _:
+                    print(f"How did you get here? (skipping {line.strip()})")
+                    continue
+
+            position %= 100  # Calculates actual position
+            count += (position == 0)  # Funny bool moment
+            # print(f"DEBUG: {position}")
+
+    print(f"Final count: {count}")
+
+if __name__ == "__main__":
+    main()
---
+++ year2025/day1/part2/solution.py
@@ -0,0 +1,29 @@
+FILE = "../.input"
+
+def main():
+    position = 50
+    count = 0
+
+    with open(FILE, 'r') as rotations_file:
+        for line in rotations_file:
+            direction = line[0]
+            value = int(line[1:])
+            # This works but is slow due to 2D for loop w/ comparisons, see if better math trick or something
+            for _ in range(value):
+                match direction:
+                    case 'L':
+                        position -= 1
+                    case 'R':
+                        position += 1
+                    case _:
+                        print(f"How did you get here? (skipping {line.strip()})")
+                        continue
+
+                position %= 100
+                count += (position == 0)
+            # print(f"DEBUG: {position}")
+
+    print(f"Final count: {count}")
+
+if __name__ == "__main__":
+    main()
