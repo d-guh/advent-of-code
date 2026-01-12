1bf21ddd375806b7991b45fbf2a85ae62f2ef540
D-Guy <dguy@dguy.net>
2026-01-11 21:29:46 -0500

    Improved day3 part2 Python solution

--- year2025/day3/part2/solution.py
+++ year2025/day3/part2/solution.py
@@ -10,26 +10,25 @@ def main():
     with open(FILE_PATH, 'r') as banks_file:
         for bank in banks_file:
             bank = bank.strip()  # Clean list
-            bank = list(map(int, bank))  # Convert to ints
-            orig_bank = bank  # Debug
-            # print(f"DEBUG: bank: {orig_bank}")
-            batteries = bank[-num_batteries:]  # Fill with specified amount of batteries from end of list
-            # print(f"DEBUG: starting batteries: {batteries}")
+            remaining_bank = list(map(int, bank))  # Convert to ints
+            # print(f"DEBUG: bank: {bank}")
+            batteries = remaining_bank[-num_batteries:]  # Fill with specified amount of batteries from end of list
+            # print(f"DEBUG: starting batteries: {''.join(map(str,batteries))}")
             
-            for cur_battery in bank[:-num_batteries][::-1]:
+            for cur_battery in remaining_bank[:-num_batteries][::-1]:
                 # print(f"DEBUG: cur_battery: {cur_battery}")
                 for i, battery in enumerate(batteries):
                     # print(f"DEBUG: {cur_battery}, {battery}")
                     if cur_battery < battery:
                         break
-                    # print(f"DEBUG: Replacing...")
+                    # print(f"DEBUG: Replacing {battery} -> {cur_battery}")
                     cur_battery, battery = battery, cur_battery
                     batteries[i] = battery
             
             combined_joltage = 0
             for joltage in batteries:
                 combined_joltage = combined_joltage * 10 + joltage
-            # print(f"DEBUG: {orig_bank}: {combined_joltage}")
+            # print(f"DEBUG: {bank}: {combined_joltage}")
             total_joltage += combined_joltage
     print(f"Total Joltage: {total_joltage}")
 
