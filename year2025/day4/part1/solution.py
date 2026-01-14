3359ee192bfef8c47d895f54b6820d7aff770ba0
D-Guy <dguy@dguy.net>
2026-01-13 20:59:45 -0500

    Completed day4 part1 Python

--- year2025/day4/part1/solution.py
+++ year2025/day4/part1/solution.py
@@ -2,9 +2,54 @@
 # Day 4: Printing Department, Part 1
 
 FILE_PATH = "../.input"
+original_diagram = []
+
+def print_diagram(diagram: list[list[bool]]) -> None:
+    for row in diagram:
+        print(''.join('@' if col else '.' for col in row))
+    print('=' * len(diagram))
+
+def check_surrounding(x: int, y: int) -> bool:
+    # print(f"DEBUG: Checking surrounding for {x},{y}")
+    count = 0
+    directions = [
+        (0, -1),  # N
+        (1, 0),   # E
+        (0, 1),   # S
+        (-1, 0),  # W
+        (1, -1),  # NE
+        (1, 1),   # SE
+        (-1, 1),  # SW
+        (-1, -1), # NW
+    ]
+
+    for dx, dy in directions:
+        nx, ny = x + dx, y + dy
+        if 0 <= nx < len(original_diagram) and 0 <= ny < len(original_diagram[0]):
+            # print(f"DEBUG: Position at {nx},{ny} (offset {dx}, {dy}) is {original_diagram[ny][nx]}")
+            if original_diagram[ny][nx]:
+                count += 1
+
+    return (count < 4)
 
 def main():
-    pass
+    accessible_rolls = 0
+
+    with open(FILE_PATH, 'r') as diagram_file:
+        for line in diagram_file:
+            bool_line = [char == '@' for char in line.strip()]
+            original_diagram.append(list(bool_line))
+
+        for y, row in enumerate(original_diagram):
+            for x, col in enumerate(row):
+                # print(f"DEBUG: {x},{y}: {original_diagram[y][x]}")
+                if col:
+                    if check_surrounding(x, y):
+                        accessible_rolls += 1
+
+        print_diagram(original_diagram)
+        print(f"Total Accessible Rolls: {accessible_rolls}")
+
 
 if __name__ == "__main__":
     main()
