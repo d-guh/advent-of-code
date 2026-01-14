8258b16c9ffda1b263e80b1938451615e9e10e0f
D-Guy <dguy@dguy.net>
2026-01-13 19:38:11 -0500

    Added day4 part1 input and starter files

---
+++ year2025/day4/part1/solution.cpp
@@ -0,0 +1,15 @@
+// Day 4: Printing Department, Part 1
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+const string FILE_PATH = "../.input";
+
+int main() {
+    return 0;
+}
---
+++ year2025/day4/part1/solution.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Day 4: Printing Department, Part 1
+
+FILE_PATH = "../.input"
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
