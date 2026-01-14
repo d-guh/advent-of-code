14fcf44df304e1720406f85d4e6d414725e89445
D-Guy <dguy@dguy.net>
2026-01-13 23:14:51 -0500

    Added day5 part1 input and starter files

---
+++ year2025/day5/part1/solution.cpp
@@ -0,0 +1,14 @@
+// Day 5: Cafeteria, Part 1
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+const string FILE_PATH = "../.input";
+
+int main() {
+    return 0;
+}
---
+++ year2025/day5/part1/solution.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Day 5: Cafeteria, Part 1
+
+FILE_PATH = "../.input"
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
