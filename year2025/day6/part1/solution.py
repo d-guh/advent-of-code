7bd87f9b438605ec49c3a0e1e5c490300cd23be6
D-Guy <dguy@dguy.net>
2026-01-15 00:04:56 -0500

    Added day6 part1 input and starter files

---
+++ year2025/day6/part1/solution.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# Day 6: Trash Compactor, Part 1
+
+FILE_PATH = "../.input"
+
+def main():
+    # Should be easy, just store in 2d list and parse, also probably an in-place solution thats more efficient
+    pass
+
+if __name__ == "__main__":
+    main()
