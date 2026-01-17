d5b388bae9ca46514821111c63e984d141df5698
D-Guy <dguy@dguy.net>
2026-01-17 16:33:53 -0500

    Completed day6 part1 Python

--- year2025/day6/part1/solution.py
+++ year2025/day6/part1/solution.py
@@ -4,8 +4,39 @@
 FILE_PATH = "../.input"
 
 def main():
-    # Should be easy, just store in 2d list and parse, also probably an in-place solution thats more efficient
-    pass
+    total = 0
+    worksheet = []
+
+    with open(FILE_PATH, 'r') as worksheet_file:
+        worksheet = [line.split() for line in worksheet_file]
+    
+    worksheet = [tuple(row[::-1]) for row in zip(*worksheet)]
+
+    # print(f"DEBUG: {worksheet}")
+
+    operator = ''
+    value = 0
+    for equation in worksheet:
+        # There's either a lapse in my knowledge or C++ is much better suited for this lol
+        for i, op in enumerate(equation):  # Operator or operand
+            if i != 0:
+                #print(f"DEBUG: op: {value} {operator} {op} = ", end='')
+                if operator == '+':
+                    value += int(op)
+                elif operator == '*':
+                    value *= int(op)
+                #print(value)
+            else:
+                operator = op
+                if operator == '+':
+                    value = 0
+                elif operator == '*':
+                    value = 1
+        #print(f"DEBUG: tot: {total} + {value} = ", end='')
+        total += value
+        #print(total)
+
+    print(f"Total: {total}")
 
 if __name__ == "__main__":
     main()
