e9d3884bc3b4c873bcb1093cee5519f99537322a
D-Guy <dguy@dguy.net>
2026-01-17 23:34:05 -0500

    Added day6 part2 C++ starter, sleep is calling

---
+++ year2025/day6/part2/solution.cpp
@@ -0,0 +1,17 @@
+// Day 6: Trash Compactor, Part 2
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+#include <iterator>
+
+using namespace std;
+
+const string FILE_PATH = "../.input";
+
+int main() {
+    return 0;
+}
