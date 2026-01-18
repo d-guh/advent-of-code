81defeda54dcbd09ae541616be6de33c9a23af0e
D-Guy <dguy@dguy.net>
2026-01-18 17:31:22 -0500

    Added day7 part1 starter files and input

---
+++ year2025/day7/part1/solution.cpp
@@ -0,0 +1,15 @@
+// Day 7: Laboratories, Part 1
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+const string FILE_PATH = "../.input";
+
+int main() {
+    unsigned long long split_count = 0;
+    return 0;
+}
---
+++ year2025/day7/part1/solution.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Day 7: Laboratories, Part 1
+
+FILE_PATH = "../.input"
+
+def main():
+    split_count = 0
+
+if __name__ == "__main__":
+    main()
