73b0a9ed1b436cae2883d91f11cfb46c3fa36649
D-Guy <dguy@dguy.net>
2026-01-22 13:58:08 -0500

    Completed day8 part1 C++; Slight improvements to Python

--- year2025/day8/part1/solution.py
+++ year2025/day8/part1/solution.py
@@ -1,31 +1,29 @@
 #!/usr/bin/env python3
 # Day 8: Playground, Part 1
 
-import collections
-
 FILE_PATH = "../.input"
 
 class UnionFind:
-    def __init__(self, size):
-        self.parent = list(range(size))
+    def __init__(self, size: int) -> None:
+        self.parent = list(range(size))  # Sequential fill
     
-    def find(self, i):
+    def find(self, i: int) -> int:
         if self.parent[i] == i:
             return i
 
         self.parent[i] = self.find(self.parent[i])  # Path compression
         return self.parent[i]
     
-    def unite(self, i, j):
-        irep = self.find(i)
-        jrep = self.find(j)
-        if irep != jrep:  # if not in same circuit
-            self.parent[irep] = jrep
+    def unite(self, i: int, j: int) -> None:
+        root_i = self.find(i)
+        root_j = self.find(j)
+        if root_i != root_j:  # if not in same circuit
+            self.parent[root_i] = root_j
 
 # Not euclidean distance, but faster, and accurate enough for ordering
 def squared_distance(point1: (int, int, int), point2: (int, int, int)) -> int:
     #return ((point2[0] - point1[0]) ** 2) + ((point2[1] - point1[1]) ** 2) + ((point2[2] - point1[2]) ** 2)
-    return sum((a - b) ** 2 for a, b in zip(point1, point2))  # A bit more generic/easy to expand
+    return sum((p2 - p1) ** 2 for p1, p2 in zip(point1, point2))  # A bit more generic/easy to expand
 
 def main():
     junction_boxes = []
@@ -34,7 +32,7 @@ def main():
         for line in junction_boxes_file:
             line = line.rstrip('\n')
             junction_boxes.append(tuple(map(int, line.split(','))))
-    junction_boxes = tuple(junction_boxes)  # Convert to tuple, prevents modifying initial list, could improve operation speed?
+    #junction_boxes = tuple(junction_boxes)  # Convert to tuple, prevents modifying initial list, no real performance gain or anything though
 
     num_boxes = len(junction_boxes)
     all_pairs = []
@@ -52,10 +50,10 @@ def main():
     for dist, i, j in all_pairs[:1000]:
         uf.unite(i, j)
 
-    circuit_sizes = collections.Counter()
+    circuit_sizes = {}
     for i in range(num_boxes):
         root = uf.find(i)
-        circuit_sizes[root] += 1
+        circuit_sizes[root] = circuit_sizes.get(root, 0) + 1
 
     all_sizes = sorted(circuit_sizes.values(), reverse=True)
     #print(f"DEBUG: Total separate circuits: {len(all_sizes)}")
