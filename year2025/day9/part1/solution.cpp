a1b0c303f7ca4508b715ca2626736f79c656b21b
D-Guy <dguy@dguy.net>
2026-01-22 20:14:48 -0500

    Added day9 part1 input and starter files

---
+++ year2025/day9/part1/solution.cpp
@@ -0,0 +1,14 @@
+// Day 9: Movie Theater, Part 1
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+static const string FILE_PATH = "../.input";
+
+int main() {
+    return 0;
+}
---
+++ year2025/day9/part1/solution.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Day 9: Movie Theater, Part 1
+
+FILE_PATH = "../.input"
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
